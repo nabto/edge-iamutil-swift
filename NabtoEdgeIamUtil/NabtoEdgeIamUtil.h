66fb0b91c82b70af0b852f21212e9fe6de475746
Ulrik Gammelby <ug@nabto.com>
2022-06-07 15:47:16 +0200

    Initial Commit

---
+++ NabtoEdgeIamUtil/NabtoEdgeIamUtil.h
@@ -0,0 +1,18 @@
+//
+//  NabtoEdgeIamUtil.h
+//  NabtoEdgeIamUtil
+//
+//  Created by Ulrik Gammelby on 07/06/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NabtoEdgeIamUtil.
+FOUNDATION_EXPORT double NabtoEdgeIamUtilVersionNumber;
+
+//! Project version string for NabtoEdgeIamUtil.
+FOUNDATION_EXPORT const unsigned char NabtoEdgeIamUtilVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NabtoEdgeIamUtil/PublicHeader.h>
+
+
